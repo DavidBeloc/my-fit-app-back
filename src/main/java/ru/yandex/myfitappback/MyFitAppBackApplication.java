506df12afdb9447207d0c773581f0a3accee60fa
David Belotserkovsky <d.belotserkovsky@gmail.com>
2025-01-25 20:59:38 +0300

    init

---
+++ src/main/java/ru/yandex/myfitappback/MyFitAppBackApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.myfitappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFitAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yandex/myfitappback/MyFitAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.myfitappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
